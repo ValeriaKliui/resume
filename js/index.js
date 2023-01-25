0045323d88b62a6ae91c55bebe814fba4b2ea3e8
Valeria Kliui <valeria14003@gmail.com>
2023-01-25 20:51:00 +0300

    make slider

--- js/index.js
+++ js/index.js
@@ -4,13 +4,13 @@ const languages = ["english", "russian", "belorussian"];
 
 function makeStatusBarProcent(lang) {
     for (const statusBar of statusBars) {
-       if (statusBar.classList.contains(languages[0])) {
-        statusBar.style.width = "60%"
+       if (statusBar.classList.contains(lang[0])) {
+        statusBar.style.width = "70%"
        }
-       else if (statusBar.classList.contains(languages[1])) {
+       else if (statusBar.classList.contains(lang[1])) {
         statusBar.style.width = "90%"
        }
-       else if (statusBar.classList.contains(languages[2])) {
+       else if (statusBar.classList.contains(lang[2])) {
         statusBar.style.width = "80%"
        }
     }
@@ -24,13 +24,40 @@ let word = document.querySelector('.about-me__title');
 const wordText = word.innerText;
 let result = '';
 let seconds = 0;
-let timerId = setInterval(toStartTyping,'200');
+const timerId = setInterval(toStartTyping,'80');
 
 function toStartTyping() {
     seconds++;
     result = result+  `${wordText.charAt(seconds-1)}`;
-    word.classList.toggle('typing');
-    word.innerText = result;
+    if (seconds%2 === 0) word.innerText = result;
+    else word.innerText = result + ' |';
     if (wordText.length === result.length) clearInterval(timerId); 
 }
 
+
+//slider
+const arrowLeft = document.querySelector('.arrow-left');
+const arrowRight = document.querySelector('.arrow-right');
+const photo = document.querySelector('.about-me__photo');
+let counter = 0;
+function changePhoto() {
+    const gallery = ['profile-pic01.jpg', 'profile-pic02.jpeg', 'profile-pic03.jpg'];
+    if (this.classList.contains('arrow-right')) counter++;
+    else counter--;
+    if (counter>=gallery.length) counter = gallery.length -1;
+    if (counter<=0) counter = 0;
+    photo.src= `assets/img/${gallery[counter]}`;
+    changeArrow.call(this, gallery);
+}
+
+function changeArrow(gallery) {
+    console.log(this);
+    if (counter === gallery.length -1 || counter===0) {
+        this.classList.add('disabled');
+    }
+    if (counter > 0) arrowLeft.classList.remove('disabled');
+    if (counter < gallery.length - 1) arrowRight.classList.remove('disabled');
+}
+
+arrowRight.addEventListener('click', changePhoto);
+arrowLeft.addEventListener('click', changePhoto);
